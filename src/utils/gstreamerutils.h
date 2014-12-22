9577ece2cfcca4cf53bdcc0fab8618e41415be2b
Marcin Kolny <marcin.kolny@gmail.com>
2014-12-22 02:22:19 +0100

    Utils: added gstreamer utils
        * .gitignore: added .deps directory from utils to ignored list
        * Makefile.am:
        * configure.ac: added utils to a build system
        * src/utils/Makefile.am: makefile of utils module
        * src/utils/gstreamerutils.{cpp|h}: implemented a few helper methods
          (get_object_info, parse_caps).

---
+++ src/utils/gstreamerutils.h
@@ -0,0 +1,47 @@
+/*
+ * gstreamerutils.h
+ *
+ *  Created on: Dec 22, 2014
+ *      Author: loganek
+ */
+
+#ifndef GSTREAMERUTILS_H_
+#define GSTREAMERUTILS_H_
+
+#include <gstreamermm.h>
+
+#include <map>
+
+namespace GstreamerStudio {
+namespace Utils {
+
+class ObjectNodeInfo
+{
+public:
+  typedef std::map<std::string, ObjectNodeInfo> MapInfo;
+private:
+  std::string value;
+  MapInfo info_map;
+public:
+  ObjectNodeInfo() {}
+  ObjectNodeInfo (const std::string& value) : value(value) {}
+  std::string get_value() const { return value; }
+  void insert(const std::string& key, const ObjectNodeInfo& obj)
+  { info_map.insert({key, obj}); }
+  MapInfo get_map() const { return info_map; }
+};
+
+class GstreamerUtils
+{
+private:
+  static ObjectNodeInfo::MapInfo parse_pad_template(const Glib::RefPtr<Gst::PadTemplate>& tpl);
+  static ObjectNodeInfo parse_caps(const Glib::RefPtr<Gst::Caps>& caps);
+public:
+  static std::map<std::string, ObjectNodeInfo> get_object_info(const Glib::RefPtr<Gst::Object>& object);
+};
+
+}
+}
+
+
+#endif /* GSTREAMERUTILS_H_ */
