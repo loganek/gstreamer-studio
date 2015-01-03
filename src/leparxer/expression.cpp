a0fd0690af95f768bf55fa7416c4e7cc6d7fe2f2
Marcin Kolny <marcin.kolny@gmail.com>
2015-01-03 16:34:39 +0100

    LeParXer: added base Expression's classes
        * .gitignore: added .deps dir of leparxer module to ignored list
        * src/leparxer/expression.{cpp|h}: added expressions:
          MethodExpression, ObjectExpression and StringExpression.

---
+++ src/leparxer/expression.cpp
@@ -0,0 +1,74 @@
+/*
+ * expression.cpp
+ *
+ *  Created on: Jan 3, 2015
+ *      Author: loganek
+ */
+
+#include "expression.h"
+
+#include <stdexcept>
+
+using namespace GstreamerStudio::LeParXer;
+
+ObjectExpression::ObjectExpression(const std::string& name)
+: child(nullptr),
+name(name)
+{
+}
+
+ObjectExpression::~ObjectExpression()
+{
+  if (child != nullptr)
+  delete child;
+}
+
+void ObjectExpression::set_child(ObjectExpression* child)
+{
+  this->child = child;
+}
+
+std::string ObjectExpression::get_name() const
+{
+  return name;
+}
+
+ObjectExpression* ObjectExpression::get_child() const
+{
+  return child;
+}
+
+MethodExpression* ObjectExpression::as_method()
+{
+  if (!is_method())
+    throw std::runtime_error("object is not a method");
+
+  return static_cast<MethodExpression*>(this);
+}
+
+bool ObjectExpression::is_method() const
+{
+  return get_type() == ExpressionType::METHOD;
+}
+
+MethodExpression::MethodExpression(const std::string& name, std::vector<ObjectExpression*>& args)
+: ObjectExpression(name), args(args)
+{}
+
+MethodExpression::~MethodExpression()
+{
+  while (args.size())
+  {
+    delete args[0];
+    args.erase(args.begin());
+  }
+}
+
+std::vector<ObjectExpression*> MethodExpression::get_args() const
+{
+  return args;
+}
+
+StringExpression::StringExpression(const std::string& name)
+: ObjectExpression(name)
+{}
---
+++ src/leparxer/expression.h
@@ -0,0 +1,79 @@
+/*
+ * expression.h
+ *
+ *  Created on: Jan 3, 2015
+ *      Author: loganek
+ */
+
+#ifndef EXPRESSION_H_
+#define EXPRESSION_H_
+
+#include <list>
+#include <string>
+#include <vector>
+
+namespace GstreamerStudio {
+namespace LeParXer {
+
+enum class ExpressionType
+{
+  OBJECT = 0,
+  METHOD = 1,
+  STRING = 2
+};
+
+class MethodExpression;
+
+class ObjectExpression
+{
+private:
+  ObjectExpression* child;
+
+protected:
+std::string name;
+
+public:
+  virtual ~ObjectExpression();
+  ObjectExpression(const std::string& name);
+
+  std::string get_name() const;
+  ObjectExpression* get_child() const;
+  MethodExpression* as_method();
+  void set_child(ObjectExpression* child);
+
+  virtual ExpressionType get_type() const
+  { return ExpressionType::OBJECT; }
+
+  bool is_method() const;
+};
+
+
+class MethodExpression : public ObjectExpression
+{
+private:
+  std::vector<ObjectExpression*> args;
+
+public:
+  MethodExpression(const std::string& name, std::vector<ObjectExpression*>& args);
+  virtual ~MethodExpression();
+
+  std::vector<ObjectExpression*> get_args() const;
+
+  ExpressionType get_type() const
+  { return ExpressionType::METHOD; }
+};
+
+
+class StringExpression : public ObjectExpression
+{
+public:
+  StringExpression(const std::string& name);
+
+  ExpressionType get_type() const
+  { return ExpressionType::STRING; }
+};
+
+}
+}
+
+#endif /* EXPRESSION_H_ */
